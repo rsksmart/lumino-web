08f099a37e7f6b3f572dfd0ae43abf7357e1f292
Jonathan <jonathan.franco.2103@gmail.com>
2021-02-16 17:58:59 -0300

    Adding missing property chainEndpoint to endpointConfig.js

--- public/static/endpointConfig.js
+++ public/static/endpointConfig.js
@@ -1,12 +1,9 @@
 const backendUrl='http://localhost:5001';
 const nodeAddress = '0x4E7eA0919a88f9103e6eE5323D24A1073d79fb0D';
 const rnsDomain = 'dev.rsk.co';
-
+const chainEndpoint = 'http://localhost:4444';
 
 window.luminoUrl = backendUrl;
 window.nodeAddress= nodeAddress;
 window.rnsDomain = rnsDomain;
-
-
-
-
+window.chainEndpoint = chainEndpoint;
